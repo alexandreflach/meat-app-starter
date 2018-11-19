c19069f70b7e57dd4ee55c3bb2c78687c1d7e47a
alexandreflach <contato@alexandreflach.com.br>
2018-11-19 21:36:08 -0200

    Operadores debounceTime e distinctUntilChanged

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -4,6 +4,8 @@ import { RestaurantsService } from './restaurants.service';
 import {trigger, state, style, transition, animate} from '@angular/animations'
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'mt-restaurants',
@@ -42,6 +44,8 @@ export class RestaurantsComponent implements OnInit {
     })
 
     this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
       .switchMap(searchTerm =>
         this.restaurantService.restaurants(searchTerm))
       .subscribe(restaurants => this.restaurants = restaurants)
